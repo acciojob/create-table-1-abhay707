7f00ac6d2cd2ff57bd078a597bca79586b48b483
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-10 20:08:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let table = document.getElementById("sampleTable");
+
 function insert_Row() {
-    //Write your code here
-  
-  
+	
+	const newRow = document.createElement("tr");
+	const newCol1 = document.createElement("td");
+	const newCol2 = document.createElement("td");
+	
+	newCol1.textContent = "New Cell1";
+	newCol2.textContent = "New Cell2";
+
+	newRow.append(newCol1);
+	newRow.append(newCol2);
+
+	table.prepend(newRow);
 }
